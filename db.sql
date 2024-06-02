fb05897021d9d6c6349985c19484ebb6571ff1bd
Miłosz Gilga <gilgamilosz451@gmail.com>
2024-06-02 21:28:39 +0200

    add sql script for creating tv_channels table

---
+++ db.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS tv_channels (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+
+    slug VARCHAR(255) NOT NULL,
+    name VARCHAR(255),
+ 
+    PRIMARY KEY (id),
+    INDEX (slug)
+)
+ENGINE=InnoDB COLLATE=utf16_polish_ci;
