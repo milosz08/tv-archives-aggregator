0a8259668b92c35476ef429a620ce580efeb187a
Miłosz Gilga <gilgamilosz451@gmail.com>
2024-06-03 14:36:50 +0200

    add create weekdays table and insert all weekdays

--- db.sql
+++ db.sql
@@ -1,3 +1,13 @@
+CREATE TABLE IF NOT EXISTS weekdays (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+
+    name VARCHAR(50) NOT NULL,
+    alias VARCHAR(5) NOT NULL,
+ 
+    PRIMARY KEY (id)
+)
+ENGINE=InnoDB COLLATE=utf16_polish_ci;
+
 CREATE TABLE IF NOT EXISTS tv_channels (
     id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
 
@@ -8,3 +18,12 @@ CREATE TABLE IF NOT EXISTS tv_channels (
     INDEX (slug)
 )
 ENGINE=InnoDB COLLATE=utf16_polish_ci;
+
+INSERT INTO weekdays (id,name,alias) VALUES
+(1,"poniedziałek", "pn"),
+(2,"wtorek", "wt"),
+(3,"środa", "śr"),
+(4,"czwartek", "czw"),
+(5,"piątek", "pt"),
+(6,"sobota", "sb"),
+(7,"niedziela", "nd");
