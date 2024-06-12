ed14e220844be1e213dc5cad4f4b3bafe2da4ed7
Miłosz Gilga <gilgamilosz451@gmail.com>
2024-06-12 02:29:01 +0200

    add scrapper and tv programs data inserting thread

--- web-scrapper/src/main/java/pl/miloszgilga/tvarchiver/webscrapper/scrap/DataScrapperThread.java
+++ web-scrapper/src/main/java/pl/miloszgilga/tvarchiver/webscrapper/scrap/DataScrapperThread.java
@@ -17,55 +17,152 @@
 package pl.miloszgilga.tvarchiver.webscrapper.scrap;
 
 import lombok.extern.slf4j.Slf4j;
-import pl.miloszgilga.tvarchiver.webscrapper.soup.TvChannel;
+import org.springframework.jdbc.core.JdbcTemplate;
+import pl.miloszgilga.tvarchiver.webscrapper.db.TvDailyProgramBatchUpdate;
+import pl.miloszgilga.tvarchiver.webscrapper.gui.MessageDialog;
+import pl.miloszgilga.tvarchiver.webscrapper.soup.*;
+import pl.miloszgilga.tvarchiver.webscrapper.state.AppState;
 import pl.miloszgilga.tvarchiver.webscrapper.state.RootState;
 
-import java.util.Random;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.TemporalAdjusters;
+import java.util.*;
 
 @Slf4j
 public class DataScrapperThread extends Thread {
 	private final RootState rootState;
+	private final MessageDialog messageDialog;
 	private final TvChannel selectedChannel;
-	private final long minDelayMs;
-	private final long maxDelayMs;
+	private final long minDelayMs, maxDelayMs;
 	private final Random random;
+	private final TvChannelDayScheduleSource dayScheduleSource;
+	private final int selectedYear;
+	private final TvChannelDetails tvChannelDetails;
+	private final long totalFetchedCount;
 
-	private boolean isScrappingActive = true;
-	private long processRecords = 0;
+	private boolean isScrappingActive;
 
-	public DataScrapperThread(RootState rootState, int minDelay) {
+	public DataScrapperThread(RootState rootState, MessageDialog messageDialog, int minDelay) {
 		super("ScrapperThread");
 		this.rootState = rootState;
+		this.messageDialog = messageDialog;
 		this.selectedChannel = rootState.getSelectedChannel();
 		minDelayMs = minDelay * 1000L;
 		maxDelayMs = rootState.getRandomness() * 1000L;
 		random = new Random();
+		dayScheduleSource = new TvChannelDayScheduleSource(selectedChannel.slug());
+		selectedYear = rootState.getSelectedYear();
+		tvChannelDetails = rootState.getTvChannelDetails();
+		totalFetchedCount = rootState.getTotalFetchedCount();
 	}
 
 	@Override
 	public void run() {
-		// init load data from db and prepare scrapper function
-		while (isScrappingActive) {
+		final JdbcTemplate jdbcTemplate = rootState.getJdbcTemplate();
+
+		final Map<Integer, TvChannelYearData> years = tvChannelDetails.years();
+		final int firstYear = Collections.min(years.keySet());
+		final int lastYear = Collections.max(years.keySet());
+
+		// only for selected year, for non-selected year, pick start and end date
+		LocalDate startDate;
+		LocalDate endDate;
+		if (selectedYear != -1) {
+			startDate = LocalDate.of(selectedYear, Month.JANUARY, 1);
+			endDate = LocalDate.of(selectedYear, Month.DECEMBER, 1).with(TemporalAdjusters.lastDayOfMonth());
+			// if its first year, pick start day
+			if (selectedYear == firstYear) {
+				startDate = tvChannelDetails.startDate();
+			} else if (selectedYear == lastYear) { // if its last year, pick end day
+				endDate = tvChannelDetails.endDate();
+			}
+		} else {
+			startDate = tvChannelDetails.startDate();
+			endDate = tvChannelDetails.endDate();
+		}
+		// check already saved dates
+		final String listSql = "SELECT schedule_date FROM tv_programs_data WHERE schedule_date BETWEEN ? AND ?";
+		final List<LocalDate> alreadySavedDates = jdbcTemplate.queryForList(listSql, LocalDate.class, startDate, endDate);
+
+		// get tv channel id
+		final String objSql = "SELECT id FROM tv_channels WHERE slug = ?";
+		final Long channelId = jdbcTemplate.queryForObject(objSql, Long.class, selectedChannel.slug());
+
+		// dates range
+		final List<LocalDate> datesRange = generateDateRange(startDate, endDate, alreadySavedDates);
+		final long totalDaysToProcess = datesRange.size();
+
+		log.info("Pre-scrap info: start: {}, end: {}, days: {}. Skipping: {} days", startDate, endDate,
+			totalDaysToProcess, alreadySavedDates.size());
+
+		isScrappingActive = true;
+		log.info("Staring scrap data from channel: {} with max delay: {}s", selectedChannel, maxDelayMs / 1000);
+
+		long totalProcessesDays = totalFetchedCount;
+		for (final LocalDate date : datesRange) {
+			if (!isScrappingActive) {
+				break;
+			}
 			final long randomnessWaitingTime = minDelayMs + random.nextLong(maxDelayMs - minDelayMs + 1);
-			log.info("Scrapping data... Iteration: {}, randomness: {}ms", ++processRecords, randomnessWaitingTime);
-			rootState.updateProgressBar((int) processRecords);
+			final List<DayScheduleDetails> details = dayScheduleSource.fetchDayScheduleDetails(date);
+			final String insertSql = """
+				INSERT INTO tv_programs_data(
+					name, description, program_type, season, episode, badge, hour_start, schedule_date,
+				    weekday, channel_id
+				) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+				""";
+			jdbcTemplate.batchUpdate(insertSql, new TvDailyProgramBatchUpdate(details, date, channelId));
 			try {
 				sleep(randomnessWaitingTime);
 			} catch (InterruptedException ex) {
 				log.error("Scrapping thread interrupted. Cause: {}", ex.getMessage());
 				break;
 			}
+			rootState.updateTotalFetchedCount(++totalProcessesDays);
+			rootState.updateChannelDetails(updateYearFetchedProgramsCount(date));
+			log.info("Add {} programs data: {} with randomness: {}s", details.size(), date, randomnessWaitingTime);
 		}
-		log.info("Stopped scrap data from: {} with processed records count: {}", selectedChannel, processRecords);
-	}
+		log.info("Stopped scrap data from: {} with processed records count: {}", selectedChannel, totalProcessesDays);
+		rootState.updateAppState(AppState.IDLE);
 
-	@Override
-	public synchronized void start() {
-		log.info("Staring scrap data from channel: {} with max delay: {}s", selectedChannel, maxDelayMs / 1000);
-		super.start();
+		boolean isEnded;
+		long processedDays;
+		if (selectedYear != -1) {
+			// check, if fetching data for selected year is ended
+			final TvChannelYearData year = rootState.getTvChannelDetails().years().get(selectedYear);
+			isEnded = year.getFetchedCount() == year.getTotalCount();
+			processedDays = year.getTotalCount();
+		} else {
+			// check for all data from all years
+			isEnded = totalProcessesDays == tvChannelDetails.daysCount();
+			processedDays = totalProcessesDays;
+		}
+		if (isEnded) {
+			messageDialog.showInfo("Scrapping ended. Processed %d days.", processedDays);
+			rootState.updateSelectedYear(-1);
+		}
 	}
 
 	public void stopScrapping() {
 		isScrappingActive = false;
+	}
+
+	private List<LocalDate> generateDateRange(LocalDate startDate, LocalDate endDate, List<LocalDate> skippedDates) {
+		final List<LocalDate> dates = new ArrayList<>();
+		LocalDate currentDate = startDate;
+		while (!currentDate.isAfter(endDate)) {
+			dates.add(currentDate);
+			currentDate = currentDate.plusDays(1);
+		}
+		dates.removeAll(skippedDates);
+		return dates;
+	}
+
+	private TvChannelDetails updateYearFetchedProgramsCount(LocalDate processingDate) {
+		final TvChannelDetails details = rootState.getTvChannelDetails();
+		final TvChannelYearData year = details.years().get(processingDate.getYear());
+		year.setFetchedCount(year.getFetchedCount() + 1);
+		return details;
 	}
 }
--- web-scrapper/src/main/java/pl/miloszgilga/tvarchiver/webscrapper/soup/TvChannelDayScheduleSource.java
+++ web-scrapper/src/main/java/pl/miloszgilga/tvarchiver/webscrapper/soup/TvChannelDayScheduleSource.java
@@ -17,13 +17,53 @@
 package pl.miloszgilga.tvarchiver.webscrapper.soup;
 
 import lombok.extern.slf4j.Slf4j;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import pl.miloszgilga.tvarchiver.webscrapper.util.Constant;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 public class TvChannelDayScheduleSource extends AbstractUrlSource {
-	public TvChannelDayScheduleSource(String channelSlug, LocalDate selectedDay) {
-		super(UrlSource.TV_CHANNEL_DAY_SCHEDULE, channelSlug, Constant.DTF.format(selectedDay));
+	public TvChannelDayScheduleSource(String channelSlug) {
+		super(UrlSource.TV_CHANNEL_DAY_SCHEDULE, channelSlug);
+	}
+
+	public List<DayScheduleDetails> fetchDayScheduleDetails(LocalDate selectedDay) {
+		url = url + "?dzien=" + Constant.DTF.format(selectedDay);
+		connectAndGet();
+
+		// get all program blocks
+		final Elements programsBlocks = rootNode.select(".atomsTvChannelEmissionTile__tileTag");
+
+		final List<DayScheduleDetails> dayScheduleDetails = new ArrayList<>();
+		for (final Element programBlock : programsBlocks) {
+
+			final Element titleNode = programBlock.selectFirst(".atomsTvChannelEmissionTile__title");
+			final Element descriptionNode = programBlock.selectFirst(".atomsTvChannelEmissionTile__lead");
+			final Element programType = programBlock.selectFirst(".atomsTvChannelEmissionTile__category");
+
+			final Element season = programBlock.selectFirst(".atomsTvChannelEmissionTile__season-value");
+			final Element episode = programBlock.selectFirst(".atomsTvChannelEmissionTile__episode-value");
+
+			final Element badge = programBlock.selectFirst(".atomsPartialLabelLabelFill__wrapper");
+			final Element timeNode = programBlock.selectFirst(".atomsTvChannelEmissionTile__emissionStartDate");
+
+			if (titleNode == null || programType == null || timeNode == null) {
+				continue; // skipping for nullable values
+			}
+			dayScheduleDetails.add(new DayScheduleDetails(
+				titleNode.text(),
+				descriptionNode == null ? null : descriptionNode.text(),
+				programType.text(),
+				season == null ? null : Integer.valueOf(season.text()),
+				episode == null ? null : Integer.valueOf(episode.text()),
+				badge == null ? null : badge.text(),
+				timeNode.text()
+			));
+		}
+		return dayScheduleDetails;
 	}
 }
