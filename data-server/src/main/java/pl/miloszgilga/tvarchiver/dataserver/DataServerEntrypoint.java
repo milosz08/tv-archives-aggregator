8a488901251200d8b46194ad443e23ca9ba57142
Miłosz Gilga <gilgamilosz451@gmail.com>
2024-06-13 12:04:30 +0200

    add mysql driver and JDBC spring boot starter (with pre-configured db source)

--- data-server/src/main/java/pl/miloszgilga/tvarchiver/dataserver/DataServerEntrypoint.java
+++ data-server/src/main/java/pl/miloszgilga/tvarchiver/dataserver/DataServerEntrypoint.java
@@ -18,7 +18,9 @@ package pl.miloszgilga.tvarchiver.dataserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
+@EnableJdbcRepositories
 @SpringBootApplication
 public class DataServerEntrypoint {
 	public static void main(String[] args) {
