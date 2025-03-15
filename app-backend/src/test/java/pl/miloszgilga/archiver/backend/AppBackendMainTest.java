885a0eab18bc446d9d4b884be1b86dbebe13ef26
Miłosz Gilga <gilgamilosz451@gmail.com>
2025-03-15 06:24:14 +0100

    fix spring boot test initiator class

---
+++ app-backend/src/test/java/pl/miloszgilga/archiver/backend/AppBackendMainTest.java
@@ -0,0 +1,11 @@
+package pl.miloszgilga.archiver.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBackendMainTest {
+  @Test
+  void contextLoads() {
+  }
+}
